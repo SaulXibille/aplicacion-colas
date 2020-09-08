e0d3b4c4e78d5d5f58525ba0e3d748e6d7b2b051
Saul <saulxibillemartinez@gmail.com>
2020-09-08 17:52:14 -0600

    Primer commit

---
+++ public/js/socket.nuevo-ticker.js
@@ -0,0 +1,23 @@
+// Comando para establecerla conexion
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al sevidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del sevidor');
+});
+
+socket.on('estadoActual', function(resp) {
+    console.log(resp);
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
